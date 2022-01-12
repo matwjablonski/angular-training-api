b92887692ee36faa6404634f81440d3c1136282b
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-01-12 13:05:31 +0100

    search

--- controllers/books.js
+++ controllers/books.js
@@ -11,4 +11,13 @@ router.get('/:id', (req, res) => {
     res.json({ data: books.find(book => book.id == req.params.id) })
 });
 
+router.get('/search/:title', (req, res) => {
+    const filtered = books.filter(book => book.title.toLowerCase().includes(req.params.title.toLowerCase()));
+    res.json({ data: [ ...filtered ] })
+});
+
+router.post('/', (req, res) => {
+    res.json({ data: [ ...books, { title: 'dasda' } ]})
+});
+
 module.exports = router;
