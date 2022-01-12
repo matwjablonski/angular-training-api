151dd3611b136c6a79acd8f69ae7c92d04566806
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-01-13 00:19:25 +0100

    saving data

--- controllers/books.js
+++ controllers/books.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const res = require('express/lib/response');
-const books = require('../books');
+const data = require('../data');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
-    res.json({ data: [...books]})
+    res.json({ data: [...data.books]})
 });
 
 router.get('/:id', (req, res) => {
-    res.json({ data: books.find(book => book.id == req.params.id) })
+    res.json({ data: data.books.find(book => book.id == req.params.id) })
 });
 
 router.get('/search/:title', (req, res) => {
-    const filtered = books.filter(book => book.title.toLowerCase().includes(req.params.title.toLowerCase()));
+    const filtered = data.books.filter(book => book.title.toLowerCase().includes(req.params.title.toLowerCase()));
     res.json({ data: [ ...filtered ] })
 });
 
 router.post('/', (req, res) => {
-    res.json({ data: [ ...books, { title: 'dasda' } ]})
+    const copyToSave = Object.assign({}, data);
+    copyToSave.books = [ ...data.books, { id: data.books.length + 1, ...req.body }];
+    fs.writeFile('./data.json', JSON.stringify(copyToSave), () => {
+        res.json({ data: [ ...copyToSave.books ]})
+    }) 
 });
 
 module.exports = router;
